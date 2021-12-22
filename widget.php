a0b9098fba081351b26118472c95cd6d9bc59a8e
riccardo <apple.newton.testa@gmail.com>
2021-12-22 21:22:11 +0100

    Interrogazione servizio REST

--- widget.php
+++ widget.php
@@ -17,6 +17,22 @@ $decoded=json_decode($json_data);
 //Contenuto dati da API
 $features=$decoded->features[0]->attributes;
 
+$stati=[
+    0=>[
+        'desc'=>'LIVELLO 0 - Solo limitazioni permanenti',
+        'colore'=>'green'
+    ],
+    1=>[
+        'desc'=>'LIVELLO 1 - Limitazioni di livello 1 (previsti 3 giorni consecutivi sopra il valore di 50 ug/m3)',
+        'colore'=>'orange'
+    ],
+    2=>[
+        'desc'=>'LIVELLO 2 - Limitazioni di livello 2 (previsti 3 giorni consecutivi sopra il valore di 75 ug/m3)',
+        'colore'=>'red'
+    ]
+    ];
+
+
 /*DEBUG per dati
  *
  * foreach ($decoded as $key=>$decode){
@@ -36,3 +52,27 @@ $features=$decoded->features[0]->attributes;
 echo '<hr><h1>';
 print_r($features);
 echo'</h1>';
+
+$nome_comune=$features->COMUNE_NOM;
+$teplate_struct='<br><span style="text-transform: uppercase; font-weight: 800">Comune di %s</span><br>';
+echo sprintf($teplate_struct,$nome_comune);
+
+$template_info="<span style='text-transform: uppercase'>Valido per %s (%s)</span><br> <span style='color:%s'>%s</span><br> ";
+$oggi=$stati[$features->LIMIT_GG_CORRENTE];
+$domani=$stati[$features->LIMIT_GG_SUCCESSIVA];
+
+echo sprintf($template_info,'oggi',date("Y-m-d",$features->DATA_OGGI/1000),$oggi['colore'],$oggi['desc']);
+echo sprintf($template_info,'domani',date("Y-m-d",$features->DATA_DOMANI/1000),$domani['colore'],$domani['desc']);
+
+$data_aggiornamento=date("Y-m-d",$features->DATA_AGG/1000);
+$teplate_struct='<br><span style="text-transform: uppercase; font-weight: 800">Data aggiornamento %s</span>';
+echo sprintf($teplate_struct,$data_aggiornamento);
+
+$url_sito=$features->URL_SITO;
+$teplate_struct='<br><a href="%s" target="_blank" style="font-weight: 800">Maggiori informazioni</a>';
+echo sprintf($teplate_struct,$url_sito);
+
+
+$data_prox=date("d m Y",$features->DATA_PROX_EMISS/1000);
+$teplate_struct='<br><span style="text-transform: uppercase; font-weight: 800">Data prossima emissione %s</span>';
+echo sprintf($teplate_struct,$data_prox);
